76b2259b9a0e9b2d94af52f216e36f9d718e7a47
jradziejewski <192462@student.ue.wroc.pl>
2024-09-18 01:16:35 +0200

    data manipulation

---
+++ data manipulation/delete_duplicate_emails.py
@@ -0,0 +1,6 @@
+import pandas as pd
+
+
+def delete_duplicate_emails(person: pd.DataFrame) -> None:
+    person.sort_values(by="id", axis=0, inplace=True)
+    person.drop_duplicates(subset=["email"], keep="first", inplace=True)
---
+++ data manipulation/nth_highest_salary.py
@@ -0,0 +1,10 @@
+import pandas as pd
+
+
+def nth_highest_salary(employee: pd.DataFrame, N: int) -> pd.DataFrame:
+    salaries = employee["salary"].drop_duplicates().sort_values(ascending=False)
+
+    if len(salaries) < N or N < 1:
+        return pd.DataFrame([{f"getNthHighestSalary({N})": None}])
+
+    return pd.DataFrame([{f"getNthHighestSalary({N})": salaries.iloc[N - 1]}])
